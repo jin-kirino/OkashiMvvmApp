4c5083726cbc2dbd9ffc81562ff332c31fa02d3e
jin-kirino <jinkirino@gmail.com>
2022-07-09 11:38:27 +0900

    first push

---
+++ MyOkashiMvvm/MyOkashiMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiMvvmApp.swift
+//  MyOkashiMvvm
+//
+//  Created by 神　樹里乃 on 2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
