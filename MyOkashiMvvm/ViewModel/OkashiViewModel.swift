47cdba77ae0c1bcb8d5d25a6fc7f0cee5be546d5
jin-kirino <jinkirino@gmail.com>
2022-07-18 16:28:43 +0900

    ViewModelの動作確認

--- MyOkashiMvvm/ViewModel/OkashiViewModel.swift
+++ MyOkashiMvvm/ViewModel/OkashiViewModel.swift
@@ -23,17 +23,16 @@ class OkashiData: ObservableObject {
     func okashiData(keyword: String) async {
         // OkashiModelをインスタンス化
         let okashiModel = OkashiModel()
-        // ●serchOkashiメソッドを呼び出し
-        self.okashiList = await okashiModel.searchOkashi(keyword: keyword)
-
         // @Publishedの変数を更新するときはメインスレッドで更新する必要がある
         DispatchQueue.main.async {
             // okashiLostのデータを初期化（前回検索データが保持されているため.removwAll())
             self.okashiList.removeAll()
-            // ●お菓子の配列okashiListへ追加
-            self.okashiList.append(self.okashiList)
-            print(self.okashiList)
+            print("remove:\(self.okashiList)")
+            
         }// DispatchQueue
+        // serchOkashiメソッドを呼び出し
+        self.okashiList = await okashiModel.searchOkashi(keyword: keyword)
+        print("append:\(self.okashiList)")
     }// seachOkashi
 }// OkashiData
 
@@ -43,4 +42,4 @@ class OkashiData: ObservableObject {
 // 半角・全角チェック
 // 未入力チェック
 // Publishing changes from background threads is not allowed; make sure to publish values from the main thread (via operators like receive(on:)) on model updates.
-// バックグラウンドスレッドからの変更の発行は許可されていません。モデルの更新時には、必ずメインスレッドから (stus) のような演算子を使って) 値を発行するようにしてください。
+// バックグラウンドスレッドからの変更の発行は許可されていません。モデルの更新時には、必ずメインスレッドから(receive(on:) のような演算子を使って) 値を発行するようにしてください。
